fe8c5ca524e9dda06a16c17c2f1b3f9a537b7bad
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2022-09-15 11:34:02 -0500

    repo: ejercicios if

---
+++ repo/031-estructuras_selectivas-ejercicios/05-formulageneral.py
@@ -0,0 +1,20 @@
+import math
+
+# Pedimos los número al usuario
+a = eval(input("Escribir en número \"a\": "))
+b = eval(input("Escribir en número \"b\": "))
+c = eval(input("Escribir en número \"c\": "))
+
+# Calculamos la formula 
+discriminante = b**2 - 4*a*c
+
+# Si el discriminante es menor a cero
+if discriminante < 0:
+    print("La solución tiene raices imaginarias.")
+else:
+    x1 = (-b + math.sqrt(discriminante)) / (2*a)
+    x2 = (-b - math.sqrt(discriminante)) / (2*a)
+    
+    # imprimimos resultado
+    print("X1 es:", x1)
+    print("X2 es:", x2)
