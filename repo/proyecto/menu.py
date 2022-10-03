c273daad671d995848730f6e27ab1760da4fb3d5
Miguel Bernal Marin <miguel.bernal.marin@gmail.com>
2022-10-02 19:12:58 -0500

    proy: salir del menu

--- repo/proyecto/menu.py
+++ repo/proyecto/menu.py
@@ -17,11 +17,15 @@ def inicial():
     Menu inicial
     """
     opcion = 0
-    while opcion != 1 or opcion != 2:
+    while opcion < 1 or opcion > 3:
         cabecera()
         print("1. Ingresar Usuario")
         print("2. Registrar Usuario\n")
+        print("3. Salir\n")
         opcion = eval(input("Opción: "))
+
+        if opcion != 1 or opcion != 2:
+            print("*** Opción invalida ***")
 
     return opcion
 
