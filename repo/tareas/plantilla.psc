b5aa52782d24ef9eeba2d97ec6cf359139726689
Miguel Bernal Marin <miguel.bernal@iteso.mx>
2024-01-22 08:44:32 -0600

    repo: agregando plantillas

---
+++ repo/tareas/plantilla.psc
@@ -0,0 +1,7 @@
+// Nombre: <Aquí pones tu nombre>
+// Correo: <Aquí pones tu correo de la escuela>
+// Carrera: <Aquí pones la carrera que cursas>
+
+Algoritmo <Nombre del Algoritmo>
+     <Cuerpo del Algoritmo>
+FinAlgoritmo
---
+++ repo/tareas/plantilla.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+#
+"""
+Nombre: <Aquí pones tu nombre>
+Correo: <Aquí pones tu correo de la escuela>
+Carrera: <Aquí pones la carrera que cursas>
+"""
+
+def main():
+    """
+    Comentario de la función
+    """
+    # Tu algoritmo va aquí
+    print("Hola Mundo")
+
+
+if __name__ == "__main__":
+    main()
+
